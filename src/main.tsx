67ecffdc5f2dfdd0877762d8699c3f7be2a54998
Lan Nguyen <ln.nguy3n@gmail.com>
2026-06-25 22:00:44 -0500

    Set up Convex authentication foundation

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+
+import { ConvexReactClient } from "convex/react";
+import { ConvexAuthProvider } from "@convex-dev/auth/react";
+
 import { router } from "./app/router";
 
 import "./styles/globals.scss";
 import "./index.css";
 
+const convex = new ConvexReactClient(
+	import.meta.env.VITE_CONVEX_URL
+);
+
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+	<StrictMode>
+		<ConvexAuthProvider client={convex}>
+			<RouterProvider router={router} />
+		</ConvexAuthProvider>
+	</StrictMode>
 );
