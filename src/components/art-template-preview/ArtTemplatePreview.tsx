5c255b430cbacadb9ea2944f43d37f4e87ad49f2
lanestrin <ln.nguy3n@gmail.com>
2026-07-27 18:56:09 -0500

    feat: add editable SVG artwork editor (#22)

---
+++ src/components/art-template-preview/ArtTemplatePreview.tsx
@@ -0,0 +1,24 @@
+import type { ArtTemplate } from "../../assets/art-templates";
+
+import styles from "./ArtTemplatePreview.module.scss";
+
+interface ArtTemplatePreviewProps {
+  template: ArtTemplate;
+  svg?: string;
+}
+
+export default function ArtTemplatePreview({
+  template,
+  svg,
+}: ArtTemplatePreviewProps) {
+  return (
+    <div
+      className={styles.preview}
+      role="img"
+      aria-label={`${template.name} artwork preview`}
+      dangerouslySetInnerHTML={{
+        __html: svg ?? template.svg,
+      }}
+    />
+  );
+}
